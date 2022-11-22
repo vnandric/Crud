7c3ae6425f2d215829f6d9c60e8e8fd6f65de78f
Valentino <valentinoandric200425@gmail.com>
2022-11-22 13:23:10 +0100

    time shit werkt nog niet

--- toevoegVerwerk.php
+++ toevoegVerwerk.php
@@ -3,39 +3,63 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+//lees de waarde uit en stop de waarden in variabelen
+$ond = $_POST['onderwerpVeld'];
+$inh = $_POST['inhoudVeld'];
+$begin = $_POST['begindatumVeld'];
+$eind = $_POST['einddatumVeld'];
+$prior = $_POST['prioritreitVeld'];
+$stat = $_POST['statusVeld'];
+
+$datumb = strtotime($begin);
+$gdatumb = date('d-m-Y', $datumb);
+
+$datume = strtotime($eind);
+$gdatume = date('d-m-Y', $datume);
+
 require "config.php";
 //is er een formulier verstuurd?
 // -> check of de knop is verstuurd:
-if (isset($_POST['verzend']) &&
-    empty($_POST['onderwerpVeld']) &&
-    empty($_POST['inhoudVeld']) &&
-    empty($_POST['begindatumVeld']) &&
-    empty($_POST['einddatumVeld']) &&
+//if (isset($_SERVER["HTTP_REFERER"]) && $_SERVER["HTTP_REFERER"] == "https://87788.stu.sd-lab.nl/crud/toevoegForm.html") {
+    
+    if (isset($_POST['verzend']) &&
+    !empty($_POST['onderwerpVeld']) &&
+    !empty($_POST['inhoudVeld']) &&
+    !empty($_POST['begindatumVeld']) &&
+    !empty($_POST['einddatumVeld']) &&
     isset($_POST['prioritreitVeld']) &&
     isset($_POST['statusVeld'])) {
-    //lees de waarde uit en stop de waarden in variabelen
-    $ond = $_POST['onderwerpVeld'];
-    $inh = $_POST['inhoudVeld'];
-    $begin = $_POST['begindatumVeld'];
-    $eind = $_POST['einddatumVeld'];
-    $prior = $_POST['prioritreitVeld'];
-    $stat = $_POST['statusVeld'];
-
-    //maak een toevoeg-query
-    $query = "INSERT INTO crud_agenda";
-    $query .= " (Onderwerp, Inhoud, Begindatum, Einddatum, Prioriteit, Status)";
-    $query .= " VALUES ('{$ond}', '{$inh}', '{$begin}', '{$eind}', {$prior}, '{$stat}')";
-
-    $result = mysqli_query($mysqli, $query);
-
-    if ($result) {
-        echo "Het item is toegevoegd<br/>";
+    
+        if (is_numeric($prior) && $prior > 1 && $prior <= 5) {
+            if ($begin == $gdatumb) {
+                //maak een toevoeg-query
+                $query = "INSERT INTO crud_agenda";
+                $query .= " (Onderwerp, Inhoud, Begindatum, Einddatum, Prioriteit, Status)";
+                $query .= " VALUES ('{$ond}', '{$inh}', '{$begin}', '{$eind}', {$prior}, '{$stat}')";
+    
+                $result = mysqli_query($mysqli, $query);
+    
+                if ($result) {
+                    echo "Het item is toegevoegd<br/>";
+                } else {
+                    echo "FOUT bij toevoegen<br/>";
+                    echo mysqli_error($mysqli);
+                }
+    
+                echo "<a href='toonagenda.php'>OVERZICHT</a>";
+            } else {
+                echo "Het formulier is niet (goed) verstuurd2<br/>";
+                echo "<a href='toevoegForm.html'>Terug</a>";
+            }
+        } else {
+            echo "Het formulier is niet (goed) verstuurd1<br/>";
+            echo "<a href='toevoegForm.html'>Terug</a>";
+        }
     } else {
-        echo "FOUT bij toevoegen<br/>";
-        echo mysqli_error($mysqli);
+        echo "Het formulier is niet (goed) verstuurd3<br/>";
+        echo "<a href='toevoegForm.html'>Terug</a>";
     }
-
-    echo "<a href='toonagenda.php'>OVERZICHT</a>";
-} else {
-    echo "Het formulier is niet (goed) verstuurd<br/>";
-}
+//} else {
+    //echo "Het formulier is niet (goed) verstuurd<br/>";
+    //echo "<a href='toevoegForm.html'>Terug</a>";
+//}

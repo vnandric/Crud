bacc4b38ff7ef5a28c9ed639e1aeefc35c0b4cae
Valentino <63399446+PyroGamer010@users.noreply.github.com>
2023-04-11 23:20:44 +0200

    Update config.php
    no user and no pass

--- config.php
+++ config.php
@@ -5,8 +5,8 @@ error_reporting(E_ALL);
 
 // database logingegevens
 $db_hostname = '127.0.0.1';
-$db_username = 'db87788';
-$db_password = 'Srecko1509';
+$db_username = '';
+$db_password = '';
 $db_database = '87788_database';
 
 // maak de database verbinding
